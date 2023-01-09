0179951e05b9facd565ea02f44867c56a0733cd7
cureseven <shiningcureseven@gmail.com>
2023-01-09 20:29:14 +0900

    feat:ideaのタイトルを返す:

--- main.go
+++ main.go
@@ -4,6 +4,21 @@ import (
 	"fmt"
 )
 
+type Idea struct {
+	id	int64
+	title	string
+	body	string
+}
+
 func main() {
-	fmt.Println("Hello, W0rld!!")
+	idea := Idea{
+		id:	1,
+		title:	"初めてのアイディア",
+		body:	"初々しいよ.",
+	}
+	idea.Get(1)
+}
+
+func (self *Idea) Get(id int64) () {
+	fmt.Println("title: ", self.title)
 }
